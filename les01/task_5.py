ca2dabaddf9ef26231b3242c4d57557d091187a4
donc1everon <it.antonov@gmail.com>
2018-07-14 19:30:55 +0300

    task_5.solved

---
+++ les01/task_5.py
@@ -0,0 +1,20 @@
+# Задание - 5
+# Пользователь вводит две буквы. Определить их порядковый номер в алфавите и рассчитать
+# число букв между ними
+#
+# Блок - схема
+# https://drive.google.com/file/d/1WKuX2a21SeLX8hGT92bw1aX-x1KTKodF/view?usp=sharing
+
+import random
+
+print('Введите 2 буквы:')
+n = input('Первая буква: ')
+m = input('Вторая буква: ')
+
+num_n = ord(n) - ord('a') + 1
+num_m = ord(m) - ord('a') + 1
+num_between = abs(num_n - num_m)
+
+print(f'Порядковый номер первой буквы - {num_n}')
+print(f'Порядковый номер второй буквы - {num_m}')
+print(f'Расстояние между первой и второй буквой - {num_between}')

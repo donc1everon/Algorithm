ba0e29b4c83e8c792ed768f3b89cf9e20265f6b9
donc1everon <it.antonov@gmail.com>
2018-07-21 12:35:39 +0300

    add 2 variant

--- les03/task_03.py
+++ les03/task_03.py
@@ -3,20 +3,29 @@
 import random
 
 massive = [random.randint(1, 100) for _ in range(10)]
-min = 100
-max = 0
+min_el= 100
+max_el= 0
 
 print(f'Начальный массив - \t\t\t{massive}')
 
 for idx, el in enumerate(massive):
-    if min > el:
-        min = el
+    if min_el> el:
+        min_el= el
         min_idx = idx
-    if max < el:
-        max = el
+    if max_el< el:
+        max_el= el
         max_idx = idx
 
 massive[min_idx], massive[max_idx] = massive[max_idx], massive[min_idx]
 
-
 print(f'Массив после замены мест - \t{massive}')
+
+# вариант 2
+i_min = massive.index(min(massive))
+i_max = massive.index(max(massive))
+
+spam = massive[i_min]
+massive[i_min] = massive[i_max]
+massive[i_max] = spam
+
+print(f'Замена мест при \nпомощи встроенных функций - {massive}')

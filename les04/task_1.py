5501c32d2d1e7077f2b5eb13ea39f7c4818e1044
donc1everon <it.antonov@gmail.com>
2018-07-26 09:43:42 +0300

    task_1 complete

--- les04/task_1.py
+++ les04/task_1.py
@@ -1,14 +1,15 @@
+# Задание - 1.
+# Проанализировать скорость и сложность одного любого алгоритма, разработанных в рамках домашнего
+# задания первых трех уроков.
 import random
 import timeit
 import cProfile
 
 
-def func_1():
-    massive = [random.randint(-10000, 10000) for _ in range(10000//3)]
-    min_el = 10000
-    max_el = -10000
-
-    print(f'Начальный массив - \t\t\t{massive}')
+def func_1(n):
+    massive = [random.randint(-n, n) for _ in range(n//3)]
+    min_el = n
+    max_el = -n
 
     for idx, el in enumerate(massive):
         if min_el > el:
@@ -20,19 +21,19 @@ def func_1():
 
     massive[min_idx], massive[max_idx] = massive[max_idx], massive[min_idx]
 
-    print(f'Массив после замены мест - \t{massive}')
 
-# 100 loops, best of 5: 38.5 nsec per loop      100
-# 1000 loops, best of 5: 34.6 nsec per loop     1000
-# 10000 loops, best of 5: 34.6 nsec per loop    10000
-# 100000 loops, best of 5: 35.5 nsec per loop   100000
+
+# 1000 loops, best of 5: 549 usec per loop      1000
+# 1000 loops, best of 5: 1.12 msec per loop     2000
+# 1000 loops, best of 5: 11.7 msec per loop     20000
 #
-#        1    0.000    0.000    0.001    0.001 task_1.py:8(func_1)
-#        1    0.000    0.000    0.001    0.001 task_1.py:9(<listcomp>)
+#         1    0.001    0.001    0.017    0.017 task_1.py:9(func_1)
+# cProfile.run('func_1(20000)')
 
-# cProfile.run('func_1()')
+num = 'num1234567890123456789012345678901234567890num1234567890123456789012345678901234567890' \
+      'num1234567890123456789012345678901234567890'
 
-num = '100000213123123123123'
+
 def func_2(n):
 
     def contra_num_rec(n):
@@ -46,17 +47,30 @@ def func_2(n):
         return n[-1] + contra_num_rec(n[1:len(n) - 1]) + n[0]
     return contra_num_rec(n)
 
-# 1000000 loops, best of 5: 34.6 nsec per loop  1000000
-# 10000000 loops, best of 5: 39.6 nsec per loop 10000000  терминал задумывается на пару секунд
-# 100000000 loops, best of 5: 35 nsec per loop  100000000 тут уже задумывается на несколько десятков секунд
+# 1000 loops, best of 5: 963 nsec per loop  'num'
+# 1000 loops, best of 5: 13 usec per loop   'num1234567890123456789012345678901234567890'
+# 1000 loops, best of 5: 38.5 usec per loop 'num1234567890123456789012345678901234567890
+#                                            num1234567890123456789012345678901234567890
+#                                            num1234567890123456789012345678901234567890')
 #
-# num = '100000'
-#         1    0.000    0.000    0.000    0.000 task_1.py:36(func_2)
-#       3/1    0.000    0.000    0.000    0.000 task_1.py:38(contra_num_rec)
 #
-# num = '100000213123123123123'
-#        1    0.000    0.000    0.000    0.000 task_1.py:36(func_2)
-#     11/1    0.000    0.000    0.000    0.000 task_1.py:38(contra_num_rec)
+#    65/1    0.000    0.000    0.000    0.000 task_1.py:44(contra_num_rec)
 #
 # cProfile.run('func_2(num)')
 
+def func_3(width, n):
+    matrix = [[random.randint(-width, width) for _ in range(n)] for _ in range(n)]
+    min_column = [width] * len(matrix)
+
+    for line in matrix:
+        for idx, el in enumerate(line):
+            if el < min_column[idx]:
+                min_column[idx] = el
+
+# 100 loops, best of 5: 183 usec per loop   100000, 10
+# 100 loops, best of 5: 4.25 msec per loop  100000, 50
+# 100 loops, best of 5: 17.1 msec per loop  100000, 100
+#
+#
+#         1    0.001    0.001    0.023    0.023 task_1.py:61(func_3)
+# cProfile.run('func_3(100000, 100)')
